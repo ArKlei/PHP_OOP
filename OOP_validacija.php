bc94ae318abaddd26c058e098f3a777dc3bc673c
Arunas <43380761+ArKlei@users.noreply.github.com>
2021-12-23 20:06:35 +0200

    Add files via upload

---
+++ OOP_validacija.php
@@ -0,0 +1,68 @@
+<?php
+
+//Validacija
+
+$fields = [
+    'name' => 'text',
+    'surname' => 'text',
+    'id' => 'int'
+]; //Schema
+
+$post = [
+    'name' => 'Arunas',
+    'surname' => 'Kleinas',
+    'id' => '37510180675'
+];
+
+//Klasė validavimui
+class Validator {
+	
+    public $post; //$_POST duomenys iš inputų
+    public $fields; //Validavimu iskirtas masyvas tokios pat struktūros
+
+	// įprastas konstruktorius su paduodamais masyvais iš inputų ir validavimo masyvo
+    public function __construct($post, $fields) {
+
+        $this->post   = $post; // šio objekto post masyvas lygus gautam masyvui per post iš inputų
+        $this->fields = $fields; //šio objekto validavimo masyvas lygus tam pačiam užduotam validavimo masyvui (nesikeičia kintamieji)
+
+		if($this->check_arg($this->post)) { // kitos funkcijos paleidimas šiam objektui su gauto masyvo iš inputų perdavimu - tikrina, jeigu yra įkritusios reikšmės/nėra tuščia, paleisti kitą funkciją - papildomą tikrinimą ir raktų spausdinimą
+			$this->process_fields(); // kitos funkcijos paleidimas šiam objektui
+		}
+    }
+
+    public function check_arg($arg) { // reikšmių buvimo tikrinimo funkcija
+
+        return !empty($arg) ? true : false; // nesant reikšmių, išmes false ir nevykdys toliau
+        
+    }
+
+    public function process_fields() { // funkcija papildomai tikrinti ar įkritę iš inputų raktai atitinka validavimo masyvo raktus
+
+        foreach($this->fields as $key => $value) { 
+
+            if($this->check_fields($key, $this->post) ) // tikrina, paleidžiant kitą funkciją su įkritusiais iš inputų duomenimis ar yra raktai. Esant - spausdina raktų pavadinimus 
+                echo $key . '<br />';
+			
+			for ($i=0; $i<3; $i++) {
+				
+				if ($key == $this->post[$i]) {
+					echo 'atitinka raktai';
+				}
+				
+			}
+        }
+    }
+
+    public function check_fields($key) { // tikrinimo funkcija, papildoma, nes dublioja check_arg()
+        return array_key_exists($key, $this->post) ? true : false; // gražina true, jeigu yra raktai
+    }
+
+}
+
+//new Validator($_POST['prekes'], $fields);
+$objektas = new Validator($post, $fields);
+
+//print_r($objektas);
+
+?>
