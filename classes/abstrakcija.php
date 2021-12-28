ee95a8cc5fefaa281486f79d66c23c8668fae46b
Arunas <43380761+ArKlei@users.noreply.github.com>
2021-12-28 20:39:33 +0200

    Index paleidžiamasis

---
+++ classes/abstrakcija.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace classes;
+
+abstract class Abstrakcija {
+	
+	abstract function data();
+	
+	abstract static function sba_text();
+	
+	abstract static function sba_www();
+}
+
+
+?>
